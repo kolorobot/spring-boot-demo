a56101474784afdc7b2803450161d52413cfa609
kolorobot <rafal.borowiec@gmail.com>
2016-04-17 18:32:24 +0200

    Initial commit

---
+++ src/main/java/pl/codeleak/sbd/SpringBootDemoApplication.java
@@ -0,0 +1,12 @@
+package pl.codeleak.sbd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDemoApplication.class, args);
+	}
+}

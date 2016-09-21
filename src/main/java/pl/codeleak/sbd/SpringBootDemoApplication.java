c904a90ee685a469fdc1780ddd2b874ddd1436a7
kolorobot <rafal.borowiec@gmail.com>
2016-09-21 23:51:26 +0200

    Tuning configuration

--- src/main/java/pl/codeleak/sbd/SpringBootDemoApplication.java
+++ src/main/java/pl/codeleak/sbd/SpringBootDemoApplication.java
@@ -2,11 +2,33 @@ package pl.codeleak.sbd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.util.Assert;
+import pl.codeleak.sbd.config.WtfConfiguration;
 
+// TODO Exclude some configurations
 @SpringBootApplication
 public class SpringBootDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootDemoApplication.class, args);
-	}
+    private WtfConfiguration wtfConfiguration;
+
+    // Works as of Spring 4.3
+    public SpringBootDemoApplication(WtfConfiguration wtfConfiguration) {
+        Assert.notNull(wtfConfiguration);
+    }
+
+    @Bean
+    public EmbeddedServletContainerCustomizer containerCustomizer() {
+        return container -> {
+            container.setSessionTimeout(60);
+            // TODO Set server header value and demonstrate
+        };
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDemoApplication.class, args);
+    }
+
+
 }
---
+++ src/main/java/pl/codeleak/sbd/config/WtfProperties.java
@@ -0,0 +1,13 @@
+package pl.codeleak.sbd.config;
+
+import lombok.Getter;
+import lombok.Setter;
+
+public class WtfProperties {
+
+    // @Size(min = 5)
+    @Getter
+    @Setter
+    private String welcomeMessage;
+
+}

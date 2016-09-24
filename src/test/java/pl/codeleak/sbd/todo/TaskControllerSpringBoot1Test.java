e753bcc78fd217221dec1a567d6a809ae48ea40a
kolorobot <rafal.borowiec@gmail.com>
2016-09-24 12:20:28 +0200

    Web API - minor fixes

--- src/test/java/pl/codeleak/sbd/todo/TaskControllerSpringBoot1Test.java
+++ src/test/java/pl/codeleak/sbd/todo/TaskControllerSpringBoot1Test.java
@@ -1,17 +1,29 @@
 package pl.codeleak.sbd.todo;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
 
-// TODO Spring Boot Test with web environment
+import static org.assertj.core.api.Assertions.assertThat;
+
+// TODO Spring Boot test with web environment
 // @RunWith(SpringRunner.class)
- public class TaskControllerSpringBoot1Test {
+// @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class TaskControllerSpringBoot1Test {
 
-    // @Autowired
+    @Autowired
     private TestRestTemplate testRestTemplate;
 
     @Test
-    public void test() {
-        testRestTemplate.getForObject("/", String.class);
+    public void createsAndGetTask() {
+        // arrange
+        Task newTask = null;
+        // act
+        Task theTask = null;
+        // assert
+        assertThat(theTask.getId()).isEqualTo(newTask.getId());
     }
 }
--- src/test/java/pl/codeleak/sbd/todo/TaskControllerSpringBoot2Test.java
+++ src/test/java/pl/codeleak/sbd/todo/TaskControllerSpringBoot2Test.java
@@ -1,23 +1,17 @@
 package pl.codeleak.sbd.todo;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.ResponseEntity;
-import org.springframework.test.context.junit4.SpringRunner;
-
-import java.util.Collections;
-import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.BDDMockito.given;
 
-// TODO Spring Boot Test with web environment
-@RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+// TODO Spring Boot Test with web environment and @MockBean
+// @RunWith(SpringRunner.class)
+// @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class TaskControllerSpringBoot2Test {
 
     @Autowired
@@ -35,7 +29,6 @@ public class TaskControllerSpringBoot2Test {
         // act
         ResponseEntity<Task> taskResponseEntity =
             testRestTemplate.getForEntity("/task/1", Task.class);
-
 
         // assert
         assertThat(taskResponseEntity.getBody().getTitle()).isEqualTo("Demo");

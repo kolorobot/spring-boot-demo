c75971310c25aa248832a36988597b166388eb1e
kolorobot <rafal.borowiec@gmail.com>
2017-02-27 21:49:09 +0100

    1.5.1.RELEASE

--- src/main/java/pl/codeleak/sbd/SpringBootDemoApplication.java
+++ src/main/java/pl/codeleak/sbd/SpringBootDemoApplication.java
@@ -2,26 +2,17 @@ package pl.codeleak.sbd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
-import org.springframework.context.annotation.Bean;
 
-// TODO Exclude some configurations
+// TODO Exclude JmxAutoConfiguration
+// TODO Add logging via Lombok
 @SpringBootApplication
 public class SpringBootDemoApplication {
 
-    // TODO Inject a custom property from application.properties
+    // TODO Inject ApplicationProperties
 
-    @Bean
-    public EmbeddedServletContainerCustomizer containerCustomizer() {
-        return container -> {
-            container.setSessionTimeout(60);
-            // TODO Set server header from injected property value and demonstrate
-        };
-    }
+    // TODO Inject a application property e.g. spring.profiles.active
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootDemoApplication.class, args);
     }
-
-
 }

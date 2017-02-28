c479545fd0f3484bdf63fb6dfc38bc0e79accf55
kolorobot <rafal.borowiec@gmail.com>
2017-02-28 22:38:00 +0100

    .

---
+++ src/main/java/pl/codeleak/sbd/contact/ContactController.java
@@ -0,0 +1,6 @@
+package pl.codeleak.sbd.contact;
+
+// TODO See templates/contact/contact-form.html to implement the controller
+public class ContactController {
+
+}
--- src/main/java/pl/codeleak/sbd/contact/ContactDetails.java
+++ src/main/java/pl/codeleak/sbd/contact/ContactDetails.java
@@ -4,17 +4,18 @@ import lombok.Data;
 import org.hibernate.validator.constraints.Email;
 import org.hibernate.validator.constraints.NotBlank;
 
+// TODO Add validation messages and a ValidationMessages bundle
 @Data
 public class ContactDetails {
 
-    @NotBlank(message = "{contact.validation.name-required}")
+    // @NotBlank(message = "{contact.validation.name-required}")
     private String name;
 
-    @NotBlank(message = "{contact.validation.email-required}")
-    @Email(message = "{contact.validation.email-valid}")
+    // @NotBlank(message = "{contact.validation.email-required}")
+    // @Email(message = "{contact.validation.email-valid}")
     private String email;
 
-    @NotBlank(message = "{contact.validation.message-required}")
+    // @NotBlank(message = "{contact.validation.message-required}")
     private String message;
 
 }
